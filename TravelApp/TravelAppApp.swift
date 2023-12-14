b2bf21d41b0a596765ef2b70d007487ce09290f7
MariamKipiani <145837678+MariamKipiani@users.noreply.github.com>
2023-12-14 21:06:54 +0400

    Initial Commit

---
+++ TravelApp/TravelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelAppApp.swift
+//  TravelApp
+//
+//  Created by user on 12/14/23.
+//
+
+import SwiftUI
+
+@main
+struct TravelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
